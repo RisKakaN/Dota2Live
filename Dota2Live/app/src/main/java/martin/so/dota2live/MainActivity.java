9256020d4c23b6f4e3f555c5b0f3db5680dfacbd
Martin So <martin.so@live.com>
2018-09-02 16:37:28 +0200

    Use fragments for the navigation items

--- Dota2Live/app/src/main/java/martin/so/dota2live/MainActivity.java
+++ Dota2Live/app/src/main/java/martin/so/dota2live/MainActivity.java
@@ -3,31 +3,30 @@ package martin.so.dota2live;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_matches);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_search);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_settings);
-                    return true;
+                case R.id.navigation_matches:
+                    fragment = new MatchesFragment();
+                    break;
+                case R.id.navigation_search:
+                    fragment = new SearchFragment();
+                    break;
+                case R.id.navigation_settings:
+                    fragment = new SettingsFragment();
+                    break;
             }
-            return false;
+            return loadFragment(fragment);
         }
     };
 
@@ -36,9 +35,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        loadFragment(new MatchesFragment());
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 }
